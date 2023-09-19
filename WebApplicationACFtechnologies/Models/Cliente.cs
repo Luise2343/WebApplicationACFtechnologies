07284e71aef400c0006fa3d65ca43577fca40167
Luis Velasco <luis.velasco@aracaristudios.com>
2023-09-19 14:56:09 -0600

    Correcion en tipo de datos.

--- WebApplicationACFtechnologies/Models/Cliente.cs
+++ WebApplicationACFtechnologies/Models/Cliente.cs
@@ -1,14 +1,23 @@
-﻿using System.Data;
+﻿using System;
 
 namespace WebApplicationACFtechnologies.Models
 {
     public class Cliente
     {
-        public string identificacion { get; set; }
+        public int identificacion { get; set; }
         public string primerNombre { get; set; }
-        public string primerApellido { get; set;}   
+        public string primerApellido { get; set; }
         public int edad { get; set; }
         public string fechaDeCreacion { get; set; }
 
+        // Propiedad adicional para el nombre completo
+        public string nombreCompleto
+        {
+            get
+            {
+                // Combina el primer nombre y el primer apellido con un espacio en blanco
+                return $"{primerNombre} {primerApellido}";
+            }
+        }
     }
 }
